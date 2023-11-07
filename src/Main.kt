c81f951247e91e9bb18c9261623a623ec68b7064
Verlygo Dmitry <56788178+ToxicButterfly@users.noreply.github.com>
2023-11-07 19:38:24 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+import Sun
+
+fun main() {
+    println(Sun.diameter)
+    println(Sun.weight)
+    println(Sun.temperatureAtSurface)
+    Sun.getATan()
+    Sun.getATan()
+}
---
+++ src/Sun.kt
@@ -0,0 +1,10 @@
+
+object Sun {
+    val diameter = "1392000 km"
+    val weight = "2*10^30 kg"
+    val temperatureAtSurface = "5772 K"
+
+    fun getATan(){
+        println("Вы чуточку загорели")
+    }
+}
